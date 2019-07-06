3a694871b8b7f4bb77066ac3d0d8e17d769dfde9
MalcolmLow83 <malcomlow@gmail.com>
2019-07-06 17:14:30 +0800

    Completion of DOM to game page

--- project-1/script.js
+++ project-1/script.js
@@ -6,72 +6,132 @@ var object = {
 
             "itemImg": ["img0","img1","img2","img3","img4","img5","img6","img7","img8","img9"],
 
-            "itemName": ["item0","item1","item2","item3","item4","item5","item6","item7","item8","item9"],
+            "itemName": ["itemName0","itemName1","itemName2","itemName3","itemName4","itemName5","itemName6","itemName7","itemName8","itemName9"],
 };
 
-//for random itemImg & itemName
+
+
+
+/*--------------level 0-----------*/
+
+//random item price
+var randomFloatNumber00 = Math.random() * (.9 - 0.1) + 0.1;
+    console.log("randomFloatNumber0: " + randomFloatNumber00);
+
+//round off to 2 decimal place
+var randomItemPrice0 = randomFloatNumber00.toFixed(1);
+    console.log("randomItemPrice0: " + randomItemPrice0);
+
+//random customerMoney
+var randomFloatNumber01 = Math.random() * (.9 - 0.1) + 0.1;
+    console.log("randomFloatNumber01: " + randomFloatNumber01);
+
+//random customerMoney round off to 1 decimal place
+var randomCustomerPay0 = randomFloatNumber01.toFixed(1);
+    console.log("randomCustomerPay0: " + randomCustomerPay0);
+
+//random number random itemImg & itemName
 var randomItem = Math.floor(Math.random() * 10);
     console.log("randomItem: " + randomItem);
 
-//random item cost
-var randomFloatNumber1 = Math.random() * (99.9 - 0.1) + 0.1;
-    console.log("randomFloatNumber: " + randomFloatNumber1);
+//DOM to game screen
 
-//round off to 1 decimal place
-var randomItemCost = randomFloatNumber1.toFixed(1);
-    console.log("randomItemCost: " + randomItemCost);
+var itemNameRan = document.querySelector("#itemNameRan");
+    itemNameRan.innerHTML = object.itemName[randomItem];
 
-//random customerMoney
-var randomFloatNumber2 = Math.random() * (99.9 - 0.1) + 0.1;
-    console.log("randomFloatNumber2: " + randomFloatNumber2);
+var itemPriceRan = document.querySelector("#itemPriceRan");
+    itemPriceRan.innerHTML = "$" + randomItemPrice0 + "0";
 
-//random customerMoney round off to 1 decimal place
-var randomCustomerPay = randomFloatNumber2.toFixed(1);
-    console.log("CustomerMoney: " + randomCustomerPay);
+var customerPayRan = document.querySelector("#customerPayRan");
+    customerPayRan.innerHTML = "$" + randomCustomerPay0 + "0";
+
+var playerChange0 = document.querySelector("#playerChange");
+    playerChange0.innerHTML = "$" + randomCustomerPay0 + "0";
+    console.log("playerChange0: " + playerChange0);
 
 //correct change formula
-var correctChange = (randomCustomerPay - randomItemCost);
-    console.log("check: " + correctChange);
-
-var playerChange = "";
+var correctChange0 = (randomCustomerPay0 - randomItemPrice0);
+    console.log("correctChange0: " + correctChange0);
 
 
-//hide page 0
+
+/*--------------level 1-----------*/
+
+
+//random item cost
+var randomFloatNumber11 = Math.random() * (99.9 - 0.1) + 0.1;
+    // console.log("randomFloatNumber1: " + randomFloatNumber1);
+
+//round off to 2 decimal place
+var randomItemCost1 = randomFloatNumber11.toFixed(2);
+    // console.log("randomItemCost1: " + randomItemCost1);
+
+
+//random customerMoney
+var randomFloatNumber12 = Math.random() * (99.9 - 0.1) + 0.1;
+    // console.log("randomFloatNumber2: " + randomFloatNumber2);
+
+//random customerMoney round off to 1 decimal place
+var randomCustomerPay1 = randomFloatNumber12.toFixed(1);
+    // console.log("CustomerMoney: " + randomCustomerPay);
+
+
+/*--------------level 1 end-----------*/
+
+
+//hide page 0 and show page 1
 var page0  = document.getElementById("page0");
 var hidePage0 = function() {
-    console.log(hidePage0)
+    // console.log(hidePage0)
     if (page0.style.display === "none") {
+
         page0.style.display = "block";
-    }   else {
+    }
+    else {
         page0.style.display = "none";
     }
 };
 
-var lover = document.querySelector("button");
-    lover.addEventListener("click", hidePage0);
+var page1  = document.getElementById("page1");
+var hidePage1 = function() {
+    // console.log(hidePage1)
+    if (page1.style.display === "none") {
+
+        page1.style.display = "none";
+    }
+    else {
+        page1.style.display = "block";
+    }
+};
+
+var button = document.querySelector("button");
+    button.addEventListener("click", hidePage0);
+    button.addEventListener("click", hidePage1);
 
 
-//hide row2a, row2b and 2c
-// var row2a = document.getElementById("row2a");
-// var row2b = document.getElementById("row2b");
-// var row2c = document.getElementById("row2c");
 
-// var hideRow2Childs = function()
+
+
+
+
+
+
+
 
 
 
 
 // logic testing
-if (randomCustomerPay < randomItemCost) {
+if (randomCustomerPay0 < randomItemPrice0) {
     console.log("not enough!!");
 }
-    else if (playerChange === correctChange){
+    else if (playerChange0 === correctChange0){
         console.log("thank you! come again!");
     }
-    else if (playerChange > 0 && (playerChange > correctChange || playerChange < correctChange)){
+    else if (playerChange0 > 0 && (playerChange0 > correctChange0 || playerChange0 < correctChange0)){
         console.log("wrong change!");
 }
-    else if (playerChange === ""){
+    else if (playerChange0 === ""){
         console.log("too slow, customer left angrily~");
     };
 
