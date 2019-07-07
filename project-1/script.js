0c67d146d21ca804c4d23dfd3de7b4225b804370
MalcolmLow83 <malcomlow@gmail.com>
2019-07-07 13:33:46 +0800

    Added start game button to hidepage0 and showpage1, added both endgame and notEnough buttons on game page, css on playerChange input

--- project-1/script.js
+++ project-1/script.js
@@ -10,48 +10,46 @@ var object = {
 };
 
 
-
-
 /*--------------level 0-----------*/
 
 //random item price
-var randomFloatNumber00 = Math.random() * (.9 - 0.1) + 0.1;
-    console.log("randomFloatNumber0: " + randomFloatNumber00);
+let randomFloatNumber00 = Math.random() * (.9 - 0.1) + 0.1;
+    // console.log("randomFloatNumber0: " + randomFloatNumber00);
 
 //round off to 1 decimal place
-var randomItemPrice0 = randomFloatNumber00.toFixed(1);
-    console.log("randomItemPrice0: " + randomItemPrice0);
+let randomItemPrice0 = randomFloatNumber00.toFixed(1);
+    // console.log("randomItemPrice0: " + randomItemPrice0);
 
 //random customerMoney
-var randomFloatNumber01 = Math.random() * (.9 - 0.1) + 0.1;
-    console.log("randomFloatNumber01: " + randomFloatNumber01);
+let randomFloatNumber01 = Math.random() * (.9 - 0.1) + 0.1;
+    // console.log("randomFloatNumber01: " + randomFloatNumber01);
 
 //random customerMoney round off to 1 decimal place
-var randomCustomerPay0 = randomFloatNumber01.toFixed(1);
-    console.log("randomCustomerPay0: " + randomCustomerPay0);
+let randomCustomerPay0 = randomFloatNumber01.toFixed(1);
+    // console.log("randomCustomerPay0: " + randomCustomerPay0);
 
 //random number random itemImg & itemName
-var randomItem = Math.floor(Math.random() * 10);
-    console.log("randomItem: " + randomItem);
+let randomItem = Math.floor(Math.random() * 10);
+    // console.log("randomItem: " + randomItem);
 
 //DOM to game screen
 
-var itemNameRan = document.querySelector("#itemNameRan");
+let itemNameRan = document.querySelector("#itemNameRan");
     itemNameRan.innerHTML = object.itemName[randomItem];
 
-var itemPriceRan = document.querySelector("#itemPriceRan");
+let itemPriceRan = document.querySelector("#itemPriceRan");
     itemPriceRan.innerHTML = "$" + randomItemPrice0 + "0";
 
-var customerPayRan = document.querySelector("#customerPayRan");
+let customerPayRan = document.querySelector("#customerPayRan");
     customerPayRan.innerHTML = "$" + randomCustomerPay0 + "0";
 
-var playerChange0 = document.querySelector("#playerChange");
+let playerChange0 = document.querySelector("#playerChange");
     playerChange0.innerHTML = "$" + randomCustomerPay0 + "0";
-    console.log("playerChange0: " + playerChange0);
+    // console.log("playerChange0: " + playerChange0);
 
 //correct change formula
-var correctChange0 = (randomCustomerPay0 - randomItemPrice0);
-    console.log("correctChange0: " + correctChange0);
+let correctChange0 = (randomCustomerPay0 - randomItemPrice0);
+    // console.log("correctChange0: " + correctChange0);
 
 
 
@@ -59,57 +57,61 @@ var correctChange0 = (randomCustomerPay0 - randomItemPrice0);
 
 
 //random item cost
-var randomFloatNumber11 = Math.random() * (99.9 - 0.1) + 0.1;
+let randomFloatNumber11 = Math.random() * (99.9 - 0.1) + 0.1;
     // console.log("randomFloatNumber1: " + randomFloatNumber1);
 
 //round off to 2 decimal place
-var randomItemCost1 = randomFloatNumber11.toFixed(2);
+let randomItemCost1 = randomFloatNumber11.toFixed(2);
     // console.log("randomItemCost1: " + randomItemCost1);
 
 
 //random customerMoney
-var randomFloatNumber12 = Math.random() * (99.9 - 0.1) + 0.1;
+let randomFloatNumber12 = Math.random() * (99.9 - 0.1) + 0.1;
     // console.log("randomFloatNumber2: " + randomFloatNumber2);
 
 //random customerMoney round off to 1 decimal place
-var randomCustomerPay1 = randomFloatNumber12.toFixed(1);
+let randomCustomerPay1 = randomFloatNumber12.toFixed(1);
     // console.log("CustomerMoney: " + randomCustomerPay);
 
 
 /*--------------level 1 end-----------*/
 
 
-//hide page 0 and show page 1
+
+
+
+//hide and show function for page 0
 var page0  = document.getElementById("page0");
-var hidepage0 = function() {
-    // console.log(hidePage0)
-    if (page0.style.display === "none") {
 
-        page0.style.display = "block";
-    }
-    else {
-        page0.style.display = "none";
-    }
+//hide page 0
+var hidePage0 = function() {
+    page0.style.display = "none";
 };
 
+//show page 0
+var showPage0 = function() {
+    page0.style.display = "block";
+}
+
+
+//hide and show function for page 1
 var page1  = document.getElementById("page1");
-var hidePage1 = function() {
-    // console.log(hidePage1)
-    if (page1.style.display === "none") {
 
-        page1.style.display = "none";
-    }
-    else {
-        page1.style.display = "block";
-    }
+//hide page 0
+var hidePage1 = function() {
+    page1.style.display = "none";
 };
 
-var button = document.querySelector("button");
-    button.addEventListener("click", hidepage0);
-    button.addEventListener("click", hidePage1);
+//show page 0
+var showPage1 = function() {
+    page1.style.display = "block";
+}
 
 
-
+//when start button is clicked, hide page 0 and show page 1
+var startButton = document.querySelector("#startButton");
+    startButton.addEventListener("click", hidePage0);
+    startButton.addEventListener("click", showPage1);
 
 
 
